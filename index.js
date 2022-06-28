6fa676a41993c33d2ce56670f105123d1796bdd6
razib994 <razibeee2016@gmail.com>
2022-06-28 16:03:54 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import stylesheets
 import './style.css';
 console.log('Bangladesh');
+console.log('git hub ok');
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Learn Javascript</h1>`;
