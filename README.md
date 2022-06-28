b0a619b5a9507132ef894c6a253a6c9fd711a7f5
razib994 <razibeee2016@gmail.com>
2022-06-28 16:02:31 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-2y8q1y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-2y8q1y)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+console.log('Bangladesh');
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Learn Javascript</h1>`;
